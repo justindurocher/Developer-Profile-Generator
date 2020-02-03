484aba1d087876dd41a547593d678f07c90dc293
Justin Durocher <justduro@gmail.com>
2020-02-03 17:44:20 -0500

    More html added and javscript ajax call

--- index.js
+++ index.js
@@ -1,28 +1,67 @@
-const fs = require("fs");
+// const fs = require("fs");
 
-const axios = require("axios");
+// const axios = require("axios");
 
-const inquirer = require("inquirer");
+// const inquirer = require("inquirer");
 
-const util = require("util");
+// // import util from "util";
 
-const converterHTML = require("pdf-puppeteer");
+// // import converterHTML from "pdf-puppeteer";
 
-const converterPDF = util.promisify(converterHTML);
+// // const converterPDF = util.promisify(converterHTML);
 
-function askQuestions() {
-    
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "username",
-            message: "What is your GitHub Username?"
-        },
-        {
-            type: "input",
-            name: "color",
-            message: "What color do you want the PDF file?"
+// function askQuestions() {
+
+//     return inquirer.prompt([
+//         {
+//             type: "input",
+//             name: "username",
+//             message: "What is your GitHub Username?"
+//         },
+//         {
+//             type: "input",
+//             name: "color",
+//             message: "What color do you want the PDF file?"
+//         }
+//     ])
+// }
+// askQuestions();
+
+function githubAjaxCall() {
+
+    let queryUrl = "https://api.github.com/users/justindurocher";
+
+    $.ajax({
+        url: queryUrl,
+        method : "GET"
+    }).then(function(response){
+
+        console.log(response);
+
+        $("img").attr("src", response.avatar_url);
+
+        $(".myNameIs").text("My name is " + response.name);
+
+        $(".current").text("I am currently employed at " + response.company);
+
+        if (response.company === null) {
+
+            $(".current").text("I am currently Unemployed");
+
         }
-    ])
+
+        $(".location").text(response.location);
+
+        $(".biography").text(response.bio);
+
+        $(".pubRepos").text(response.public_repos);
+
+        $(".numFollow").text(response.followers);
+
+    });
+
+
 }
-askQuestions();
+
+githubAjaxCall();
+
